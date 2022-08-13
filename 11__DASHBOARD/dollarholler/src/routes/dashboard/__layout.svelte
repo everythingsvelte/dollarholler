9da2816785ddfcb4e5b67a3a62292178bb0cc475
Amy Dutton <amy@ahhacreative.com>
2022-08-13 08:36:24 -0500

    ✨ Added Lesson 11, Dashboard Layout

---
+++ 11__DASHBOARD/dollarholler/src/routes/dashboard/__layout.svelte
@@ -0,0 +1,30 @@
+<div class="grid min-h-screen grid-cols-12 gap-x-16 bg-whisper">
+  <header class="col-span-3 bg-daisyBush text-center">
+    <div class="mt-10 mb-24">
+      <a href="/dashboard/invoices"
+        ><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a
+      >
+    </div>
+    <nav>
+      <ul class="font-sansSerif list-none text-2xl font-bold">
+        <li>
+          <a href="/dashboard/invoices">Invoices</a>
+        </li>
+        <li><a href="/dashboard/invoices">Clients</a></li>
+        <li><a href="/dashboard/invoices">Settings</a></li>
+        <li><a href="/dashboard/invoices">Logout</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="col-span-8 pt-10"><slot><!-- optional fallback --></slot></main>
+</div>
+
+<style lang="postcss">
+  nav ul li {
+    @apply mb-6;
+  }
+
+  nav ul li a {
+    @apply text-white hover:text-goldenFizz;
+  }
+</style>
