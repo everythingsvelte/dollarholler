c8e881e35a2387a42eac9aca4a36db5e2f46e05a
Amy Dutton <amy@ahhacreative.com>
2022-08-19 10:30:09 -0500

    ✨ Updates to Lesson 5, new router system

--- 05__INSTALLING-TAILWIND/dollarholler/svelte.config.js
+++ 05__INSTALLING-TAILWIND/dollarholler/svelte.config.js
@@ -5,9 +5,11 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
 		adapter: adapter()
