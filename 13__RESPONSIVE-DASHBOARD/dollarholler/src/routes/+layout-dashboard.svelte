b83dd4e082e7f8f9cd3267786dbb90b1b7b025a8
Amy Dutton <amy@ahhacreative.com>
2022-08-19 17:36:14 -0500

    🚧 WIP: Set up lesson 13 folder

---
+++ 13__RESPONSIVE-DASHBOARD/dollarholler/src/routes/+layout-dashboard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<div class="grid min-h-screen grid-cols-12 bg-whisper md:gap-x-16">
+  <Navbar />
+
+  <main class="col-span-12 md:col-span-8 md:pt-10">
+    <slot><!-- optional fallback --></slot>
+  </main>
+</div>
