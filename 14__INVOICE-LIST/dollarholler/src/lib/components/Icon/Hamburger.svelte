191211ae02560bc9a119559fefcaf70481bb19c4
Amy Dutton <amy@ahhacreative.com>
2022-08-25 14:56:40 -0500

    ✨ Added lesson 14

---
+++ 14__INVOICE-LIST/dollarholler/src/lib/components/Icon/Hamburger.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let width: number = 24,
+    height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M24 6.56249H0V5.06248H24V6.56249Z" />
+  <path d="M24 12.75H0V11.25H24V12.75Z" />
+  <path d="M24 18.9375H0V17.4375H24V18.9375Z" />
+</svg>
---
+++ 14__INVOICE-LIST/dollarholler/src/lib/components/Search.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Search from '$lib/components/Icon/Search.svelte';
+</script>
+
+<div class="relative flex w-full items-baseline">
+  <div class="mr-2 mt-auto text-pastelPurple"><Search /></div>
+  <input
+    class="border-b-2 w-72 border-dashed border-pastelPurple bg-transparent font-sansSerif text-xl text-black outline-none"
+    type="text"
+    name="search"
+    placeholder="Search by Keyword"
+  />
+  <button class="font-sansSerif text-xl font-black text-pastelPurple">Search</button>
+</div>
