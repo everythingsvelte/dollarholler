bb65aab7c09f56eca793206cd4cb04e8bcdb2492
Amy Dutton <amy@ahhacreative.com>
2022-08-27 19:08:45 -0500

    ✨ Lesson 17 complete

---
+++ 17__ADDITIONAL-OPTIONS/dollarholler/src/lib/components/Icon/Send.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let width: number = 24,
+    height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M2.01 21L23 12L2.01 3L2 10L17 12L2 14L2.01 21Z" />
+</svg>
