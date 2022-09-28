c7a02989792ebfb0d2a6431b2c41b090f53a2140
Amy Dutton <amy@ahhacreative.com>
2022-09-28 06:29:26 -0500

    🔥 Removed old lesson files

--- 19__PORTAL/dollarholler/prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-  tailwindConfig: './tailwind.config.cjs',
-  useTabs: false,
-  singleQuote: true,
-  trailingComma: 'none',
-  printWidth: 100
-};
