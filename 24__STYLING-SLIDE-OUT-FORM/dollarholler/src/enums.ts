06bf4efca8fd86ced54970e5215689e49b913a33
Amy Dutton <amy@ahhacreative.com>
2022-10-01 13:03:06 -0500

    ✨ Lesson 24 Completed

---
+++ 24__STYLING-SLIDE-OUT-FORM/dollarholler/src/enums.ts
@@ -0,0 +1,10 @@
+export enum InvoiceStatus {
+  draft = 'draft',
+  sent = 'sent',
+  paid = 'paid'
+}
+
+export enum ClientStatus {
+  active = 'active',
+  archived = 'archived'
+}
---
+++ 24__STYLING-SLIDE-OUT-FORM/dollarholler/src/lib/components/Portal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  function portal(node: HTMLElement) {
+    let target: HTMLBodyElement | null;
+
+    function update() {
+      target = document.querySelector('body');
+      target?.appendChild(node);
+      node.hidden = false;
+    }
+
+    function destroy() {
+      if (node.parentNode) node.parentNode.removeChild(node);
+    }
+
+    update();
+
+    return { update, destroy };
+  }
+</script>
+
+<div hidden use:portal>
+  <slot><!-- optional fallback --></slot>
+</div>
