69ddd9b5d4f809aa9b0dec3e153bb818d5c5ad57
Amy Dutton <amy@ahhacreative.com>
2022-10-04 13:26:18 -0500

    ✨ Lesson 27 Finished

---
+++ 27__ADDING-REMOVING-LINE-ITEMS/dollarholler/src/routes/(dashboard)/invoices/LineItemRow.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import Trash from '$lib/components/Icon/Trash.svelte';
+
+  export let lineItem: LineItem;
+
+  let dispatch = createEventDispatcher();
+</script>
+
+<div class="invoice-line-item border-b-2 border-fog py-2">
+  <div>
+    <input class="line-item" type="text" name="description" />
+  </div>
+
+  <div>
+    <input class="line-item text-right" type="number" name="unitPrice" step="0.01" min="0" />
+  </div>
+
+  <div>
+    <input class="line-item text-center" type="number" name="quantity" min="0" />
+  </div>
+
+  <div>
+    <input class="line-item text-right" type="number" name="amount" step="0.01" min="0" />
+  </div>
+
+  <div>
+    <button
+      on:click|preventDefault={() => {
+        dispatch('removeLineItem', lineItem.id);
+      }}
+      class="center h-10 w-10 text-pastelPurple hover:text-lavenderIndigo"><Trash /></button
+    >
+  </div>
+</div>
+
+<style lang="postcss">
+  input[type='text'],
+  input[type='number'] {
+    @apply h-10 w-full border-b-2 border-dashed border-stone-300;
+  }
+
+  input[type='text'] {
+    @apply font-sansSerif text-xl font-bold;
+  }
+
+  input[type='number'] {
+    @apply font-mono text-base;
+  }
+
+  input[type='text']:focus,
+  input[type='number']:focus {
+    @apply border-solid border-lavenderIndigo outline-none;
+  }
+</style>
