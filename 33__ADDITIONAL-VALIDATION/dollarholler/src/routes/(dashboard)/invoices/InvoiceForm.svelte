d262a4a2de869fe27c5657fd989b3b44f076dc82
Amy Dutton <amy@ahhacreative.com>
2022-11-14 15:40:52 -0600

    ✨ Completed lesson 33

--- 33__ADDITIONAL-VALIDATION/dollarholler/src/routes/(dashboard)/invoices/InvoiceForm.svelte
+++ 33__ADDITIONAL-VALIDATION/dollarholler/src/routes/(dashboard)/invoices/InvoiceForm.svelte
@@ -45,7 +45,8 @@
     {#if !isNewClient}
       <label for="client">Client</label>
       <div class="flex items-end gap-x-5">
-        <select name="client" id="client">
+        <select name="client" id="client" required={!isNewClient}>
+          <option />
           {#each $clients as client}
             <option value={client.id}>{client.name}</option>
           {/each}
@@ -63,7 +64,7 @@
     {:else}
       <label for="NewClient">New Client</label>
       <div class="flex items-end gap-x-5">
-        <input type="text" name="newClient" />
+        <input type="text" name="newClient" required={isNewClient} />
         <div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
         <Button
           label="Existing Client"
@@ -88,7 +89,7 @@
     <div class="field col-span-6 grid gap-x-5" transition:slide>
       <div class="field col-span-6">
         <label for="email">Client's Email</label>
-        <input type="email" name="email" id="email" />
+        <input type="email" name="email" id="email" required={isNewClient} />
       </div>
 
       <div class="field col-span-6">
