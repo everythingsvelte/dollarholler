46269015a35f8e645ed6f2485a504101a8d0f9e2
Amy Dutton <amy@ahhacreative.com>
2022-11-22 11:58:30 -0600

    ✨ Finished Lesson 45

---
+++ 45__COPY-LINK/dollarholler/prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.cjs',
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100
+};
---
+++ 45__COPY-LINK/dollarholler/src/lib/components/CircledAmount.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let label: string;
+  export let amount: string;
+</script>
+
+<div class="center float-right flex min-h-[6rem] min-w-[16rem] gap-x-4 bg-circle bg-no-repeat">
+  <div class="font-bold">{label}</div>
+  <div class="font-mono text-xl font-bold">{amount}</div>
+</div>
