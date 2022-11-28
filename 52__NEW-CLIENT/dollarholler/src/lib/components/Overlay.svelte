29d20924d118fe930816596266845cebd32ca213
Amy Dutton <amy@ahhacreative.com>
2022-11-27 20:53:32 -0600

    ✨ Lesson 52 completed

---
+++ 52__NEW-CLIENT/dollarholler/src/lib/components/Overlay.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let className: string = '';
+</script>
+
+<svelte:head>
+  <style lang="postcss">
+    body {
+      @apply overflow-hidden;
+    }
+  </style>
+</svelte:head>
+
+<div class={`fixed inset-0 z-overlay bg-blueGem opacity-60 ${className}`} />
