a068dfa98b88d84d88381825200ec7a9d5e693f6
Amy Dutton <amy@ahhacreative.com>
2022-12-02 17:51:02 -0600

    🚧 WIP: Set up Lesson 56

---
+++ 56__STYLING-SETTINGS/dollarholler/src/lib/components/Search.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Search from '$lib/components/Icon/Search.svelte';
+</script>
+
+<div class="relative flex w-full items-baseline">
+  <div class="mr-2 mt-auto text-pastelPurple"><Search /></div>
+  <input
+    type="text"
+    name="search"
+    placeholder="Search by keyword"
+    class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
+  />
+  <button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Search</button>
+</div>
