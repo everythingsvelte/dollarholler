1a5617252dae4cb6a6baf759822a7c4b7721165d
Amy Dutton <amy@ahhacreative.com>
2022-12-02 20:16:56 -0600

    ✨ Finished lesson 59, logout

---
+++ 59__LOGOUT/dollarholler/src/routes/(dashboard)/clients/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import Button from '$lib/components/Button.svelte';
+  import Search from '$lib/components/Search.svelte';
+  import ClientRowHeader from './ClientRowHeader.svelte';
+  import ClientRow from './ClientRow.svelte';
+  import { clients, loadClients } from '$lib/stores/ClientStore';
+  import { onMount } from 'svelte';
+  import BlankState from './BlankState.svelte';
+  import SlidePanel from '$lib/components/SlidePanel.svelte';
+  import ClientForm from './ClientForm.svelte';
+
+  let isClientFormShowing = false;
+
+  const closePanel = () => {
+    isClientFormShowing = false;
+  };
+
+  onMount(() => {
+    loadClients();
+  });
+</script>
+
+<svelte:head><title>Clients | The Dollar Holler</title></svelte:head>
+
+<div
+  class="md:gap-7-4 mb-7 flex flex-col-reverse items-start justify-between gap-y-6 md:flex-row md:items-center lg:mb-16"
+>
+  <!-- search field -->
+  {#if $clients.length > 0}
+    <Search />
+  {:else}
+    <div />
+  {/if}
+
+  <!-- new invoice button -->
+  <div>
+    <Button
+      label="+ Client"
+      onClick={() => {
+        isClientFormShowing = true;
+      }}
+    />
+  </div>
+</div>
+
+<!-- clients -->
+<div>
+  {#if $clients === null}
+    Loading...
+  {:else if $clients.length <= 0}
+    <BlankState />
+  {:else}
+    <!--  client header row -->
+    <ClientRowHeader />
+
+    <!-- client rows -->
+    <div class="flex flex-col-reverse">
+      {#each $clients as client}
+        <ClientRow {client} />
+      {/each}
+    </div>
+  {/if}
+</div>
+
+{#if isClientFormShowing}
+  <SlidePanel on:closePanel={closePanel}>
+    <ClientForm {closePanel} />
+  </SlidePanel>
+{/if}
