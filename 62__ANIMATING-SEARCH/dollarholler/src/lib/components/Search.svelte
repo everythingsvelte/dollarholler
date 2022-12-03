0de261eb09be4b7c26803798082b389a8574c3cf
Amy Dutton <amy@ahhacreative.com>
2022-12-03 15:16:39 -0600

    ✨ Finished lesson 62, animating search

--- 62__ANIMATING-SEARCH/dollarholler/src/lib/components/Search.svelte
+++ 62__ANIMATING-SEARCH/dollarholler/src/lib/components/Search.svelte
@@ -8,7 +8,25 @@
     type="text"
     name="search"
     placeholder="Search by keyword"
-    class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
+    class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent pr-[80px] font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] md:pr-0 lg:w-72 lg:text-xl"
   />
-  <button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Search</button>
+  <button
+    class="pointer-events-none absolute left-8 transform font-sansSerif text-lg font-black text-pastelPurple transition-transform lg:text-xl"
+    >Search</button
+  >
 </div>
+
+<style lang="postcss">
+  ::placeholder {
+    color: transparent;
+  }
+  input:not(:placeholder-shown) + button,
+  input:focus + button {
+    @apply pointer-events-auto left-auto right-0 translate-x-[calc(100%-65px)] text-right text-lavenderIndigo md:right-auto md:left-0 md:translate-x-[16rem] lg:translate-x-80;
+  }
+
+  input + button:focus,
+  input:focus + button:hover {
+    @apply text-daisyBush;
+  }
+</style>
