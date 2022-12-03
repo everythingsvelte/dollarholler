43030aeb72db655e9a0accbc48f32ae2a79be60a
Amy Dutton <amy@ahhacreative.com>
2022-12-03 15:31:32 -0600

    ✨ Finished lesson 64, custom page transition for individual invoice

--- 64__PAGE-TRANSITION/dollarholler/src/routes/(dashboard)/invoices/[id]/+layout@.svelte
+++ 64__PAGE-TRANSITION/dollarholler/src/routes/(dashboard)/invoices/[id]/+layout@.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
   import { afterNavigate, goto } from '$app/navigation';
   import Arrow from '$lib/components/Icon/Arrow.svelte';
 
@@ -18,7 +19,7 @@
 />
 
 <div class="h-full min-h-screen w-screen bg-whisper pt-16 pb-32 lg:pt-12">
-  <main class="mx-auto min-h-screen max-w-screen-lg">
+  <main class="mx-auto min-h-screen max-w-screen-lg" transition:fly={{ y: 500, duration: 250 }}>
     <a href={previousPage ? previousPage : '/invoices'} class="fixed top-7 left-7 text-pastelPurple"
       ><Arrow /></a
     >
