39af0dbe9b4526f309fb5bb99e5be3be080b74c8
Amy Dutton <amy@ahhacreative.com>
2022-12-02 20:10:11 -0600

    ✨ Finished Lesson 58, Login Page

---
+++ 58__LOGIN/dollarholler/src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  let email: string, password: string;
+</script>
+
+<h1 class="mb-4 text-[2.5rem] font-black text-goldenFizz">Login</h1>
+
+<form>
+  <div class="field">
+    <label for="email" class="text-goldenFizz">Email Address</label>
+    <input type="email" name="email" placeholder="Your Email" bind:value={email} />
+  </div>
+  <div class="field">
+    <div class="flex justify-between">
+      <label for="password" class="text-goldenFizz">Password</label>
+      <a href="/forgot-password" class="text-sm text-white underline hover:no-underline">Forgot?</a>
+    </div>
+    <input type="password" placeholder="Your password" bind:value={password} />
+  </div>
+  <div class="field">
+    <button
+      type="submit"
+      class="mt-6 min-h-[3.5rem] w-full rounded-lg bg-robinEggBlue py-3 px-4 text-xl font-bold text-daisyBush transition-all duration-200 hover:-translate-y-1 hover:shadow-lg"
+    >
+      Let's do this!
+    </button>
+    <p class="mt-4 text-center text-sm text-white">
+      <a href="/signup" class="hover-no-underline underline">Don't have an account yet?</a>
+    </p>
+  </div>
+</form>
