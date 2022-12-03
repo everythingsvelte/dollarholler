4657c1c9785dd52b44040a125685debc818774c9
Amy Dutton <amy@ahhacreative.com>
2022-12-03 15:53:01 -0600

    ✨ Finished 65, Click outside

---
+++ 65__CLICK-OUTSIDE/dollarholler/src/routes/(auth)/reset-password/+page.svelte
@@ -0,0 +1,16 @@
+<h1 class="auth-heading">Reset my Password</h1>
+
+<form>
+  <div class="field">
+    <label for="newPassword" class="text-goldenFizz">New Password</label>
+    <input type="password" name="newPassword" required />
+  </div>
+  <div class="field">
+    <label for="confirmPassword" class="text-goldenFizz">Confirm Password</label>
+    <input type="password" name="confirmPassword" required />
+  </div>
+  <button type="submit" class="auth-button">Update my password!</button>
+  <p class="mt-4 text-center text-sm text-white">
+    <a href="/login" class="underline hover:no-underline">I'm ready to login</a>
+  </p>
+</form>
