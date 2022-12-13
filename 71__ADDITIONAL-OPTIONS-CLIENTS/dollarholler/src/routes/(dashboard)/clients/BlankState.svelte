f850b148be83e1ab13d69c3126095c9623ae676c
Amy Dutton <amy@ahhacreative.com>
2022-12-13 09:45:32 -0600

    🚧 WIP: Initialized lesson 71

---
+++ 71__ADDITIONAL-OPTIONS-CLIENTS/dollarholler/src/routes/(dashboard)/clients/BlankState.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import BlankRow from '$lib/components/BlankRow.svelte';
+  import ClientRowHeader from './ClientRowHeader.svelte';
+</script>
+
+<div>
+  <div class="-mt-8 md:-mt-6 lg:-mt-14">
+    <h2
+      class="mb-10 bg-arrowLeftUp bg-left bg-no-repeat pt-14 pl-24 text-left font-handwriting text-2xl font-bold text-caribbeanGreen md:bg-arrowRightUp md:bg-right md:pl-0 md:pr-28 md:text-right md:text-4xl"
+    >
+      Add Your First Client
+    </h2>
+  </div>
+  <!-- Client header -->
+  <ClientRowHeader className="text-fog" />
+
+  <!-- blank rows -->
+  <BlankRow />
+  <BlankRow />
+  <BlankRow />
+</div>
