8b9f241c1ef2b2edb8eb7ad410ad743c26f067a6
Amy Dutton <amy@ahhacreative.com>
2022-12-13 08:25:37 -0600

    🚧 WIP: Initialized Lesson 67

---
+++ 67__STYLING-OPTIONS/dollarholler/src/lib/components/SnackbarMessage.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { snackbar } from '$lib/stores/SnackbarStore';
+  import { onMount } from 'svelte';
+  import { tweened } from 'svelte/motion';
+  export let message: string = '';
+
+  let progress = tweened(100, { duration: 3000 });
+
+  onMount(async () => {
+    await progress.set(0);
+    snackbar.remove();
+  });
+</script>
+
+<div class="whitespace-nowrap">
+  {message}
+</div>
---
+++ 67__STYLING-OPTIONS/dollarholler/src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,23 @@
+<h1 class="auth-heading">Sign Up</h1>
+<form>
+  <div class="field">
+    <label for="fullName" class="text-goldenFizz">Full Name</label>
+    <input type="text" name="fullName" id="fullName" required />
+  </div>
+  <div class="field">
+    <label for="email" class="text-goldenFizz">Email Address</label>
+    <input type="email" name="email" id="email" required />
+  </div>
+  <div class="field">
+    <label for="password" class="text-goldenFizz">Password</label>
+    <input type="password" name="password" id="password" required />
+  </div>
+  <div class="field">
+    <label for="confirmPassword" class="text-goldenFizz">Confirm Password</label>
+    <input type="password" name="confirmPassword" id="confirmPassword" required />
+  </div>
+  <button type="submit" class="auth-button">Count me in!</button>
+  <p class="mt-4 text-center text-sm text-white">
+    <a href="/login" class="underline hover:no-underline">Already have an account?</a>
+  </p>
+</form>
