44e8e87d3601d66520bc77f4169afa09a8e03441
Amy Dutton <amy@ahhacreative.com>
2022-12-13 20:15:07 -0600

    🚧 WIP: Initialized lesson 80

---
+++ 80__POPULATING-DB/dollarholler/src/lib/components/Icon/ThreeDots.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let width: number = 24,
+    height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="5" cy="12.5" r="2" />
+  <circle cx="12" cy="12.5" r="2" />
+  <circle cx="19" cy="12.5" r="2" />
+</svg>
---
+++ 80__POPULATING-DB/dollarholler/src/lib/components/Tag.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let label: 'draft' | 'sent' | 'current' | 'late' | 'paid' | 'archive' = 'draft';
+  export let className = '';
+</script>
+
+<div
+  class={`text-md w-20 rounded-full border-1 text-center font-bold capitalize ${className}`}
+  class:inverted={label === 'draft'}
+  class:success={label === 'sent' || label === 'current'}
+  class:alert={label === 'late'}
+  class:archive={label === 'paid' || label === 'archive'}
+>
+  {label}
+</div>
+
+<style lang="postcss">
+  .inverted {
+    @apply border-pastelPurple text-pastelPurple;
+  }
+
+  .success {
+    @apply border-robinEggBlue bg-robinEggBlue text-blueGem;
+  }
+
+  .alert {
+    @apply border-scarlet bg-scarlet text-goldenFizz;
+  }
+
+  .archive {
+    @apply border-pastelPurple bg-pastelPurple text-white;
+  }
+</style>
