57c7dea9dace24a507094bb35fe9bc27d9842d23
Amy Dutton <amy@ahhacreative.com>
2022-12-20 09:32:44 -0600

    🚧 WIP: Initialized Lesson 83

---
+++ 83__ADDING-A-CLIENT/dollarholler/src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Alert from '$lib/components/Alert.svelte';
+  let email: string,
+    password: string,
+    alertMessage = '';
+</script>
+
+<h1 class="auth-heading">Login</h1>
+
+<form>
+  <Alert message={alertMessage} />
+  <div class="field">
+    <label for="email" class="text-goldenFizz">Email Address</label>
+    <input type="email" name="email" placeholder="Your Email" bind:value={email} />
+  </div>
+  <div class="field">
+    <div class="flex justify-between">
+      <label for="password" class="text-goldenFizz">Password</label>
+      <a href="/forgot-password" class="text-sm text-white underline hover:no-underline">Forgot?</a>
+    </div>
+    <input type="password" placeholder="Your password" bind:value={password} />
+  </div>
+  <div class="field">
+    <button type="submit" class="auth-button"> Let's do this! </button>
+    <p class="mt-4 text-center text-sm text-white">
+      <a href="/signup" class="hover-no-underline underline">Don't have an account yet?</a>
+    </p>
+  </div>
+</form>
