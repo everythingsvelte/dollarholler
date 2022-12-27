8148ee13448d9a4398c535bb34fc2f4fdf6385e2
Amy Dutton <amy@ahhacreative.com>
2022-12-27 16:03:39 -0600

    🚧 WIP: Protect pages client side

---
+++ 94__PROTECT-ROUTES-CLIENT-SIDE/dollarholler/src/lib/components/Icon/Activate.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let width: number = 24,
+    height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M20.55 5.22L19.16 3.54C18.88 3.21 18.47 3 18 3H6C5.53 3 5.12 3.21 4.85 3.55L3.46 5.22C3.17 5.57 3 6.01 3 6.5V19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V6.5C21 6.01 20.83 5.57 20.55 5.22ZM12 9.5L17.5 15H14V17H10V15H6.5L12 9.5ZM5.12 5L5.94 4H17.94L18.87 5H5.12Z"
+  />
+</svg>
---
+++ 94__PROTECT-ROUTES-CLIENT-SIDE/dollarholler/src/routes/(dashboard)/clients/NoSearchResults.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import BlankRow from '$lib/components/BlankRow.svelte';
+  import ClientRowHeader from './ClientRowHeader.svelte';
+</script>
+
+<div class="mt-20 lg:-mt-14">
+  <div class="ml-2 mb-10 bg-arrowLeftUp bg-no-repeat pl-28 pt-14">
+    <h2 class="font-handwriting text-4xl font-bold text-caribbeanGreen">Bummer!</h2>
+    <p class="text-xl font-bold text-caribbeanGreen">
+      We didn't find anything that matched what you were looking for.
+    </p>
+  </div>
+
+  <ClientRowHeader className="!text-fog" />
+  <BlankRow />
+  <BlankRow />
+  <BlankRow />
+</div>
