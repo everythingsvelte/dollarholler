1d8dff2a73deca00ac6ea0570c4f89629fdff5a9
Amy Dutton <amy@ahhacreative.com>
2023-01-21 14:39:49 -0600

    🚧 WIP: Setup Lesson 95

---
+++ 95__PROTECT-ROUTES-SERVER-SIDE/dollarholler/src/lib/components/Overlay.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+  export let className: string = '';
+</script>
+
+<svelte:head>
+  <style lang="postcss">
+    body {
+      @apply overflow-hidden;
+    }
+  </style>
+</svelte:head>
+
+<div class={`fixed inset-0 z-overlay bg-blueGem opacity-60 ${className}`} transition:fade />
