8e2d504f109000e358e1597f804bb44e57a7efb5
Amy Dutton <amy@ahhacreative.com>
2023-02-05 21:20:54 -0600

    ✨ Finished Lesson 97, updating the password

---
+++ 97__HANDLE-PASSWORD-UPDATE/dollarholler/src/lib/layouts/AuthLayout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Snackbar from '$lib/components/Snackbar.svelte';
+</script>
+
+<Snackbar />
+
+<div class="min-h-screen w-full bg-daisyBush bg-center md:bg-auth">
+  <div
+    class="grid h-screen w-full auto-rows-min grid-cols-12 px-4 md:auto-rows-fr md:gap-5 xl:px-0"
+  >
+    <div
+      class="col-span-12 pt-8 md:col-span-6 md:my-auto lg:col-span-4 lg:col-start-3 xl:col-span-3 xl:col-start-4"
+    >
+      <img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" />
+    </div>
+    <div class="col-span-12 md:col-span-6 md:my-auto lg:col-span-4 xl:col-span-3">
+      <slot><!-- optional fallback --></slot>
+    </div>
+  </div>
+</div>
