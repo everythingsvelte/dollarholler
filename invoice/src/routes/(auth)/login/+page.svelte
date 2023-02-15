58ac7aef830d0749f0759983b1cd3cef60762add
alessandro-giuzio <giuzio@icloud.com>
2023-02-15 16:43:09 +0100

    login

--- invoice/src/routes/(auth)/login/+page.svelte
+++ invoice/src/routes/(auth)/login/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-  let email: string, password: string;
+  import Alert from '$lib/components/Alert.svelte';
+  let email: string,
+    password: string,
+    alertMessage = '';
 </script>
 
-<h1 class="mb-4 text-[2.5rem] font-black text-goldenFizz">Login</h1>
+<h1 class="auth-heading">Login</h1>
 
 <form>
+  <Alert message={alertMessage} />
   <div class="field">
     <label for="email" class="text-goldenFizz">Email Address</label>
     <input type="email" name="email" placeholder="Your Email" bind:value={email} />
@@ -17,12 +21,7 @@
     <input type="password" name="password" placeholder="Your Password" bind:value={password} />
   </div>
   <div class="field">
-    <button
-      type="submit"
-      class="mt-6 min-h-[3.5rem] w-full rounded-lg bg-robinEggBlue py-3 px-4 text-xl font-bold text-daisyBush transition-all duration-200 hover:-translate-y-1 hover:shadow-lg"
-    >
-      Let's do this!
-    </button>
+    <button type="submit" class="auth-button"> Let's do this! </button>
     <p class="mt-4 text-center text-sm text-white">
       <a href="/signup" class="underline hover:no-underline">Don't have an account yet?</a>
     </p>
