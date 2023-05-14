6b5a19d80929217464c5a4f35c62ae827b2afa20
Amy Haywood Dutton <me@amyhaywood.com>
2023-05-14 10:15:12 -0500

    🚧 WIP: Initialized Lesson 95.2

---
+++ 95-2__REVISIT-THE-LOGIN-PAGE-FOR-SERVER-SIDE/dollarholler/src/lib/components/Icon/Check.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let width: number = 24,
+    height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M9.80022 23.4013L0 13.6011L4.2832 9.31542L9.02697 14.0568L19.0672 0L24 3.52207L9.80022 23.4013Z"
+  />
+</svg>
---
+++ 95-2__REVISIT-THE-LOGIN-PAGE-FOR-SERVER-SIDE/dollarholler/src/lib/components/Icon/Hamburger.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let width: number = 24,
+    height: number = 24;
+</script>
+
+<svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M24 6.75007H0V5.25005H24V6.75007Z" />
+  <path d="M24 12.7501H0V11.2501H24V12.7501Z" />
+  <path d="M24 18.7502H0V17.2502H24V18.7502Z" />
+</svg>
